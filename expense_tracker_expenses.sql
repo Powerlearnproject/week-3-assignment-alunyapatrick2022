a23051f32d1d905d759fde5554165c69a25887c2
alunyapatrick2022 <patrickalunya106@gmail.com>
2024-07-03 14:04:45 +0300

    [Check the .pdf document for the answers to my SQL challenges commands]

--- expense_tracker_expenses.sql
+++ expense_tracker_expenses.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `expense_tracker` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `expense_tracker`;
 USE `expense_tracker`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
@@ -31,7 +31,7 @@ CREATE TABLE `expenses` (
   `amount` decimal(10,2) NOT NULL,
   `date` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
